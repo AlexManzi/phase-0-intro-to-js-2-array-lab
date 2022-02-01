2076b39afb865a2ad57a63a6e88f94f5b410f73d
Alex Manzi <alex.manzi22@gmail.com>
2022-02-01 17:01:30 -0500

    commit tree 60b69f8dfca6637a2e165ca997d8acce5408fd7f

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 let cats = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendCat(name) {
-    cats.push(name);
+    return cats.push(name)
 }
+
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
+    return cats.unshift(name)
 }
-function destructivelyRemoveLastCat() {
-    cats.pop(name)
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
 }
-function destructivelyRemoveFirstCat() {
-    cats.shift(name)
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
 }
 function appendCat(name) {
-    let newCats = cats.slice();
-    newCats.push(name);
-    return newCats;
+    let newCats = cats.slice(name);
+    newCats.push(name)
+    return newCats
 }
+
 function prependCat(name) {
-    let newCats = cats.slice();
-    newCats.unshift(name);
-    return newCats;
+    let newCats = cats.slice(name)
+    newCats.unshift(name)
+    return newCats
 }
-function removeLastCat() {
-    let newCats = cats.slice();
-    newCats.pop(name);
-    return newCats;
+
+function removeLastCat(name) {
+    let newCats = cats.slice(name)
+    newCats.pop(name)
+    return newCats
 }
-function removeFirstCat() {
-    let newCats = cats.slice();
-    newCats.shift(name);
-    return newCats;
+
+function removeFirstCat(name) {
+    let newCats = cats.slice(name)
+    newCats.shift(name)
+    return newCats
 }
